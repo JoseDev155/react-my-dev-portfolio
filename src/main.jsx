a877f96047bbb533a0dedf6fc848808691e85759
Jose <jeduramos7001@gmail.com>
2026-01-03 22:42:37 -0600

    Fix src/main.jsx: Change the import of the CSS filename

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import './index.css';
+import './style.css';
 import { StrictMode } from 'react';
 
 // Entry point of the application
