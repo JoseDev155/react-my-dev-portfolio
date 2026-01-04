7f370bcc4cfb549de8dbfbe1aa0ce2ae210bf3c9
Jose <jeduramos7001@gmail.com>
2026-01-04 03:04:16 -0600

    Change style.css to index.css in src/

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import './style.css';
+import './index.css';
 import { StrictMode } from 'react';
 
 // Entry point of the application
