6ec5332d4732a5b179a7e192813e9139ac5d7aea
Jose <jeduramos7001@gmail.com>
2026-01-07 16:39:13 -0600

    Update src/pages/HomePage.jsx: Change relative paths

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
-import { ThemeToggleComponent } from '../components/themes/ThemeToggleComponent';
-import { BackgroundComponent } from '../components/themes/BackgroundComponent';
-import { NavbarLayout } from '../layouts/NavbarLayout';
-import { OverviewComponent } from '../components/sections/OverviewComponent';
+import { ThemeToggleComponent } from '@/components/themes/ThemeToggleComponent';
+import { BackgroundComponent } from '@/components/themes/BackgroundComponent';
+import { NavbarLayout } from '@/layouts/NavbarLayout';
+import { OverviewComponent } from '@/components/sections/OverviewComponent';
 
 export const HomePage = () => {
   return (
